46faa19b96128e4e90c890bea69adfc680134f65
kilosamagali <kmagali120@gmail.com>
2016-02-04 11:34:03 +0100

    Initial Commit

---
+++ KiKi/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KiKi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "One Step at a Time";
+        }
+    }
+}
